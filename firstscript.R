cb1e37833acaa54854aeb252efa0f323837816d5
Aubrey Annis <acannis@umich.edu>
2020-10-13 17:16:19 -0400

    Add second line
    Add a second line that is even more useful.

--- firstscript.R
+++ firstscript.R
@@ -1,3 +1,8 @@
 # gitignore is where we put things we don't care about / don't want to be seen
+# notice Git tab to right
+# staging area of Git allows you to view your files
+# committing the files gives them to Git so that they're save
+# click Commit button and write git Commit Messages in present tense
 
 print("This is my awesome R script")
+print("This is a second line")
